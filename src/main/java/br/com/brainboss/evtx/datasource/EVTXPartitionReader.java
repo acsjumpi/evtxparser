cf973a802ad15d4de256b8d06e7652355ca1207e
Alan Silva <alan.silva@databricks.com>
2023-09-19 18:58:00 -0300

    Internal XML parser methods created instead of use external libraries like XStream

--- src/main/java/br/com/brainboss/evtx/datasource/EVTXPartitionReader.java
+++ src/main/java/br/com/brainboss/evtx/datasource/EVTXPartitionReader.java
@@ -8,11 +8,12 @@ import br.com.brainboss.evtx.parser.FileHeader;
 import br.com.brainboss.evtx.parser.FileHeaderFactory;
 import br.com.brainboss.evtx.parser.MalformedChunkException;
 import br.com.brainboss.evtx.parser.Record;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.StaxDriver;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.connector.read.PartitionReader;
 import org.apache.spark.sql.types.StructType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import scala.collection.JavaConverters;
 
 import java.io.*;
@@ -21,6 +22,9 @@ import java.util.*;
 import java.util.function.Function;
 
 import org.apache.log4j.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class EVTXPartitionReader implements PartitionReader<InternalRow> {
 
@@ -65,18 +69,31 @@ public class EVTXPartitionReader implements PartitionReader<InternalRow> {
 
     @Override
     public boolean next() {
-        return fileheader.hasNext();
+        if (chunkheader.hasNext())
+            return true;
+        if (fileheader.hasNext()) {
+            try {
+                chunkheader = fileheader.next();
+            } catch (MalformedChunkException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return false;
+        }
+        return chunkheader.hasNext();
     }
 
     @Override
     public InternalRow get() {
         log.debug("EVTXPartitionReader::get joined");
-        List<Object> xmlValues = new ArrayList<>();
+        Map<String, Object>xmlValue;
 
         try {
-            while (fileheader.hasNext()) {
-                chunkheader = fileheader.next();
-                while (chunkheader.hasNext()) {
+            //while (fileheader.hasNext()) {
+                //chunkheader = fileheader.next();
+                //while (chunkheader.hasNext()) {
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     //BufferedOutputStream out = new BufferedOutputStream(baos);
                     XmlRootNodeHandler rootNodeHandler = (XmlRootNodeHandler) rootNodeHandlerFactory.create(baos);
@@ -85,20 +102,62 @@ public class EVTXPartitionReader implements PartitionReader<InternalRow> {
                     rootNodeHandler.close();
                     log.debug(baos.toString());
 
-                    XStream xs = new XStream(new StaxDriver());
-                    xs.registerConverter(new MapEntryConverter());
-                    xs.alias("Events", Map.class);
-                    Object xmlValue = xs.fromXML(baos.toString());
+                    //XStream xs = new XStream(new StaxDriver());
+                    //xs.registerConverter(new MapEntryConverter());
+                    //xs.alias("Events", Map.class);
+                    xmlValue = (Map<String, Object>) convertNodesFromXml(baos.toString());
                     log.debug(xmlValue);
-                    xmlValues.add(xmlValue);
-                }
-            }
+                //}
+            //}
         } catch (MalformedChunkException | IOException e) {
             log.debug(String.valueOf(e));
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+        return InternalRow.apply(JavaConverters.asScalaIteratorConverter(Arrays.asList(xmlValue.values().toArray()).iterator()).asScala().toSeq());
+    }
 
-        return InternalRow.apply(JavaConverters.asScalaIteratorConverter(xmlValues.iterator()).asScala().toSeq());
+    public static Object convertNodesFromXml(String xml) throws Exception {
+
+        InputStream is = new ByteArrayInputStream(xml.getBytes());
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(is);
+        return createMap(document.getDocumentElement());
+    }
+
+    public static Object createMap(Node node) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node currentNode = nodeList.item(i);
+            String name = currentNode.getNodeName();
+            Object value = null;
+            if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+                value = createMap(currentNode);
+            }
+            else if (currentNode.getNodeType() == Node.TEXT_NODE) {
+                return currentNode.getTextContent();
+            }
+            if (map.containsKey(name)) {
+                Object os = map.get(name);
+                if (os instanceof List) {
+                    ((List<Object>)os).add(value);
+                }
+                else {
+                    List<Object> objs = new LinkedList<Object>();
+                    objs.add(os);
+                    objs.add(value);
+                    map.put(name, objs);
+                }
+            }
+            else {
+                map.put(name, value);
+            }
+        }
+        return map;
     }
 
     @Override
